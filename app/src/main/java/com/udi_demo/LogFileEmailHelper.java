d7fff601a784c8afb6e79edb0eb5caee12416535
Yog <developer.yog33@gmail.com>
2023-10-28 01:01:57 +0530

    UDI demo app updated with .log file
    * Send email with app logs

---
+++ app/src/main/java/com/udi_demo/LogFileEmailHelper.java
@@ -0,0 +1,116 @@
+package com.udi_demo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * <p>
+ * Add this to your app/build.gradle -> dependencies closure:
+ * debugCompile 'com.github.tony19:logback-android-core:1.1.1-6'
+ * debugCompile 'com.github.tony19:logback-android-classic:1.1.1-6'
+ * <p>
+ * Change email address LogFileEmailHelper#LOG_FILE_RECEIVER_EMAIL_ADDRESS
+ * <p>
+ * Add attached ‘logback.xml’ to ‘app/src/main/assets’ directory and make sure it’s included in the build.
+ */
+public class LogFileEmailHelper {
+    // CHANGE THIS
+    private static final String LOG_FILE_RECEIVER_EMAIL_ADDRESS = "udaykale35@gmail.com";
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogFileEmailHelper.class);
+
+    public static void sendMailWithLogFiles(Activity activity, String textField, String subjectField) {
+        String title = "Send Log files";
+
+        Intent mailIntent = new Intent(Intent.ACTION_SEND_MULTIPLE);
+        mailIntent.setType("message/rfc822");
+        mailIntent.putExtra(Intent.EXTRA_EMAIL,
+                new String[]{LOG_FILE_RECEIVER_EMAIL_ADDRESS});
+        mailIntent.putExtra(Intent.EXTRA_SUBJECT, subjectField);
+        mailIntent.putExtra(Intent.EXTRA_TEXT, textField);
+        mailIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        /* get all existing logfiles */
+        List<File> logFiles = findLogFiles(activity);
+
+        ArrayList<Uri> uris = new ArrayList<Uri>();
+        for (File file : logFiles) {
+            Uri u = FileProvider.getUriForFile(activity, "com.udi_demo.provider", file);
+            uris.add(u);
+        }
+        mailIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+
+        LOGGER.debug("Dispatching mail to {} with {} log files", LOG_FILE_RECEIVER_EMAIL_ADDRESS, uris.size());
+
+        activity.startActivityForResult(Intent.createChooser(mailIntent, title), 0 /* dummy */);
+    }
+
+    private static List<File> findLogFiles(final @NonNull Context context) {
+        File[] matchedFiles = new File[0];
+        List<File> movedFiles = new ArrayList<File>();
+
+        File dataDir = context.getApplicationContext().getFilesDir();
+
+        final Pattern p = Pattern.compile("^logback_log.*");
+
+        if (dataDir != null && dataDir.exists()) {
+            matchedFiles = dataDir.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return p.matcher(name).matches();
+                }
+            });
+
+            /* copy the files to external storage */
+            for (File toMove : matchedFiles) {
+                try {
+                    File finalFile = moveFileToExternalStorage(context, toMove);
+                    movedFiles.add(finalFile);
+                } catch (Exception ex) {
+                    // exception is already logged
+                }
+
+            }
+
+        }
+
+        return movedFiles;
+    }
+
+    private static File moveFileToExternalStorage(final Context context, final File baseFile) throws IOException {
+        File extDir = context.getApplicationContext().getExternalFilesDir(null);
+        File newFile = new File(extDir, baseFile.getName());
+        FileChannel outputChannel = null;
+        FileChannel inputChannel = null;
+        try {
+            outputChannel = new FileOutputStream(newFile).getChannel();
+            inputChannel = new FileInputStream(baseFile).getChannel();
+            inputChannel.transferTo(0, inputChannel.size(), outputChannel);
+        } catch (Exception ex) {
+            // could not copy this file, try next!
+            LOGGER.debug("Could not copy logfile to external storage. Source: {} destination: {} exception: {}", baseFile.getAbsolutePath(), newFile.getAbsolutePath(),
+                    ex);
+        } finally {
+            if (inputChannel != null) {
+                inputChannel.close();
+            }
+            if (outputChannel != null) {
+                outputChannel.close();
+            }
+        }
+        return newFile;
+    }
+}
--- app/src/main/java/com/udi_demo/MainActivity.kt
+++ app/src/main/java/com/udi_demo/MainActivity.kt
@@ -3,7 +3,6 @@ package com.udi_demo
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -11,22 +10,32 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import org.slf4j.LoggerFactory
+
 
 class MainActivity : AppCompatActivity() {
     var logginUser = ""
-
+    val logger = LoggerFactory.getLogger(MainActivity::class.java);
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // SLF4J
         setContentView(R.layout.activity_main)
         val loginButton = findViewById<Button>(R.id.button)
+        val emailLogsBtn = findViewById<Button>(R.id.email_logs_btn)
         val loginUser = findViewById<EditText>(R.id.login_name_et)
         val logincc = findViewById<LinearLayout>(R.id.login_cc)
         val messageText = findViewById<EditText>(R.id.editTextText2)
+        emailLogsBtn.setOnClickListener {
+            LogFileEmailHelper.sendMailWithLogFiles(this, "Check this .log file", "Log Files-UDI")
+        }
         loginButton.setOnClickListener {
             logginUser = loginUser.text.toString()
             if (logginUser.isEmpty()) {
                 loginUser.error = "Empty"
             } else {
+                logger.debug("\t..........UserName:$logginUser...........")
+//                Log.i(TAG,"\n\t..........UserName:$logginUser...........")
                 this.currentFocus?.let { it1 -> hideKeyboard(it1) }
                 loginUser.error = null
                 FirebaseAnalytics.setUser(logginUser)
@@ -41,13 +50,16 @@ class MainActivity : AppCompatActivity() {
         super.onBackPressed()
         this.finish()
     }
+
     override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
         super.onKeyDown(keyCode, event)
         val unicodeChar = event?.unicodeChar?.toChar()
         if (logginUser.isNotEmpty()) {
             val bundle = Bundle()
-            bundle.putString("KeyCode_$keyCode",unicodeChar.toString())
-            Log.i("MainActivity", "Key code:$keyCode, char:$unicodeChar")
+            bundle.putString("KeyCode_$keyCode", unicodeChar.toString())
+//           Log.i(TAG,"Key pressed->\nKeyCode=$keyCode \n Key Character=${unicodeChar.toString()}")
+            logger.debug("Key code:$keyCode, char:$unicodeChar")
+//            Log.i("MainActivity", "Key code:$keyCode, char:$unicodeChar")
             FirebaseAnalytics.logEvent(FirebaseEvents.KEYBOARD_KEY_PRESS, bundle)
         }
         return true
